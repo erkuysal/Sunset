21fb78a02f9e4de9ea7abe336a359e5c96aee853
Feyzullah <f.erkuysal@gmail.com>
2024-05-01 21:47:49 +0300

    Night/ Backend

---
+++ night/dawn/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    #path('sunrise/', views.sunrise, name="sunrise"),
+    #path('noon/', views.noon, name="noon"),
+    #path('sunset/', views.sunset, name="sunset"),
+]
