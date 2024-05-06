4de59425df52aadea3ecda77fb339219a4ece703
Feyzullah <f.erkuysal@gmail.com>
2024-05-06 15:57:24 +0300

    IssueNO:1 , Backend routing doesn't change frontend routings.

--- day/src/apps/Home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <p>This is Vue, Home.</p>
-</template>
-<script setup lang="ts">
-</script>
--- day/src/apps/Sunrise.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <p>This is Vue, Sunrise.</p>
-</template>
-<script setup lang="ts">
-</script>
