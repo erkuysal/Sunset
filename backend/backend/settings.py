29ad138e60822d964b27d3a1ab96fff36fe44ae6
Feyzullah <f.erkuysal@gmail.com>
2024-05-17 21:50:38 +0300

    Context Processing Method For Dynamic Links

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,6 +73,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # ----------------- LINK PROCESSORS -----------------
+                'first.utilities.context_processors.register_nav_links',
             ],
         },
     },
---
+++ backend/first/utilities/context_processors.py
@@ -0,0 +1,13 @@
+def register_nav_links(request):
+    nav_links = [
+    ]
+
+    if request.user.is_authenticated:
+        nav_links.append({'url': 'sign-out', 'name': 'Sign Out'})
+    else:
+        nav_links.append({'url': 'sign-in', 'name': 'Sign In'})
+        nav_links.append({'url': 'sign-up', 'name': 'Sign Up'})
+
+    return {
+        'nav_links': nav_links
+    }
